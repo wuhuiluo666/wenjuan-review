c7ba3ce35ab1d70f774ec22a5d2288972f2df07f
wuhuiluo666 <510426343@qq.com>
2024-01-04 18:12:55 +0800

    latest coomit

--- src/pages/ManageQuestion/list.tsx
+++ src/pages/ManageQuestion/list.tsx
@@ -10,114 +10,70 @@ import { useSearchParams } from 'react-router-dom'
 const PAGE_SIZE = 10
 
 export const List = () => {
-    const [started, setStarted] = useState(true)
+    const [started,setStarted] = useState(true)
     const [searchParams] = useSearchParams()
     const [page, setPage] = useState(1)
     const [total, setTotal] = useState(0)
-    const [questionList, setQuestionList] = useState([
-        {
-            answerCount: 78,
-            createdAt:
-                "1973-12-29 11:09:57",
-            isDeleted:
-                false,
-            isPublished
-                :
-                false,
-            isStar
-                :
-                false,
-            title
-                :
-                "面难性她基按入",
-            _id
-                :
-                "210000199610092564"
-        },
-        {
-            answerCount
-                :
-                72,
-            createdAt
-                :
-                "1990-02-04 11:08:09",
-            isDeleted
-                :
-                false,
-            isPublished
-                :
-                false,
-            isStar
-                :
-                false,
-            title
-                :
-                "地制局须",
-            _id
-                :
-                "450000200803285821"
+    const [questionList, setQuestionList] = useState([])
+    const hasMoreData = page <= Math.round(total / PAGE_SIZE) + 1
+    // 加载问卷数据
+    const { run: loadQuestionList, loading } = useRequest(async () => {
+        const data = await getQuestionList({
+            page,
+            pageSize: 10,
+            keyword: searchParams.get('keyword') || ''
+        })
+        return data
+    }, {
+        manual: true,
+        onSuccess: (res: any) => {
+            const { list: resultList = [], total } = res
+            setQuestionList(questionList.concat(resultList))
+            setTotal(total)
+            setPage(page + 1)
         }
-    ])
-    // const hasMoreData = page <= Math.round(total / PAGE_SIZE) + 1
-    // // 加载问卷数据
-    // const { run: loadQuestionList, loading } = useRequest(async () => {
-    //     const data = await getQuestionList({
-    //         page,
-    //         pageSize: 10,
-    //         keyword: searchParams.get('keyword') || ''
-    //     })
-    //     return data
-    // }, {
-    //     manual: true,
-    //     onSuccess: (res: any) => {
-    //         console.log('res', JSON.parse(JSON.stringify(res.list)))
-    //         const { list: resultList = [], total } = res
-    //         setQuestionList(questionList.concat(resultList))
-    //         setTotal(total)
-    //         setPage(page + 1)
-    //     }
-    // })
-    // const { run: debounceLoadQuestionList } = useDebounceFn(() => {
-    //     // 文档区域高度
-    //     const clientHeight = window.innerHeight
-    //     // 卷曲的高度
-    //     const scrollTop = document.body.scrollTop || document.documentElement.scrollTop
-    //     // 所有内容的高度
-    //     const scrollHeight = document.body.scrollHeight
-    //     if (scrollHeight <= clientHeight + scrollTop) {
-    //         loadQuestionList()
-    //         setStarted(false)
-    //     }
-    // },
-    //     {
-    //         wait: 500
-    //     })
-    // // 一进入页面请求列表数据
-    // useEffect(() => {
-    //     setQuestionList([])
-    //     setStarted(true)
-    //     setPage(1)
-    //     setTotal(0)
-    //     debounceLoadQuestionList()
-    // }, [searchParams])
-    // // 控制滑动时请求次数，并且监听page变化时在重新触发,一进入的时候是0
-    // // 只有在page改变的时候加载并且触底才会加载
-    // useEffect(() => {
-    //     if (hasMoreData) {
-    //         window.addEventListener('scroll', debounceLoadQuestionList)
-    //     }
-    //     return () => {
-    //         window.removeEventListener('scroll', debounceLoadQuestionList)
-    //     }
-    // }, [page])
-    // const loadMoreElement = () => {
-    //     if (loading) {
-    //         return <Spin />
-    //     }
-    //     if (total === 0) return <Empty description={'没有数据'} />
-    //     if (!hasMoreData) return <Typography.Title level={3} style={{ margin: 0 }}>没有更多数据了</Typography.Title>
-    //     return <div>加载更多</div>
-    // }
+    })
+    const { run: debounceLoadQuestionList } = useDebounceFn(() => {
+        // 文档区域高度
+        const clientHeight = window.innerHeight
+        // 卷曲的高度
+        const scrollTop = document.body.scrollTop || document.documentElement.scrollTop
+        // 所有内容的高度
+        const scrollHeight = document.body.scrollHeight
+        if (scrollHeight <= clientHeight + scrollTop) {
+            loadQuestionList()
+            setStarted(false)
+        }
+    },
+        {
+            wait: 500
+        })
+    // 一进入页面请求列表数据
+    useEffect(() => {
+        setQuestionList([])
+        setStarted(true)
+        setPage(1)
+        setTotal(0)
+        debounceLoadQuestionList()
+    }, [searchParams])
+    // 控制滑动时请求次数，并且监听page变化时在重新触发,一进入的时候是0
+    // 只有在page改变的时候加载并且触底才会加载
+    useEffect(() => {
+        if (hasMoreData) {
+            window.addEventListener('scroll', debounceLoadQuestionList)
+        }
+        return () => {
+            window.removeEventListener('scroll', debounceLoadQuestionList)
+        }
+    }, [page])
+    const loadMoreElement = () => {
+        if (loading) {
+            return <Spin />
+        }
+        if (total === 0) return <Empty description={'没有数据'} />
+        if (!hasMoreData) return <Typography.Title level={3} style={{ margin: 0 }}>没有更多数据了</Typography.Title>
+        return <div>加载更多</div>
+    }
     return <>
         <div className={styles.header}>
             <div className={styles.left}>
@@ -133,8 +89,7 @@ export const List = () => {
             }
         </div>
         <div className={styles.footer}>
-            加载更多
-            {/* {loadMoreElement()} */}
+            {loadMoreElement()}
         </div>
     </>
 }
